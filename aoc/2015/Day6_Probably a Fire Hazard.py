505db59b62b7d17d0c11acf043bd2246a1b501d6
Justin Izatt <JustinI@dhicorp.com>
2021-09-23 15:58:07 -0600

    Create multi dimentional array

--- aoc/2015/Day6_Probably a Fire Hazard.py
+++ aoc/2015/Day6_Probably a Fire Hazard.py
@@ -1,12 +1,20 @@
-filePath = "C:\\W\\pythonplayground\\aoc\\2015\\aoc2015_day6_input.txt"
+import re
+
+filePath = "C:\\W\\python\\aoc\\2015\\aoc2015_day6_input.txt"
 
 #3https://www.educba.com/multidimensional-array-in-python/
-##lightGrid = [1000][1000]
+c = 1000
+r = 1000
+lightGrid = [ [False] * c for i in range(r) ]
 
 def Part1():
     f = open(filePath, "r")
     lines = f.read()
     
+    for x in lines.split('\n'):
+        stuff = re.search("(turn on|toggle|turn off)\ (\d+)\,(\d+)\ through\ (\d+)\,(\d+)", x)
+        print(stuff)
+
     print("Part 1")
 
 def Part2():
@@ -18,14 +26,14 @@ def Part2():
 def turnOn(xLow, xHigh, yLow, yHigh):
     for x in range(xLow, xHigh):
       for y in range(yLow, yHigh):
-        lightGrid[x][y] = 1
+        lightGrid[x][y] = True
 
 def turnOff(xLow, xHigh, yLow, yHigh):
     for x in range(xLow, xHigh):
       for y in range(yLow, yHigh):
-        lightGrid[x][y] = 0
+        lightGrid[x][y] = False
 
 if __name__ == "__main__":
-    turnOff(0, 999, 0, 999)
+    ##turnOff(0, 999, 0, 999)
     Part1()
     Part2()
